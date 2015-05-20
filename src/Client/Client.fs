abd3bdeb6ed71ad9b267267ad0ce73ea5eb7e286
Jallah <puncher@gmx.net>
2015-05-20 19:57:14 +0200

    .gitignore is now working ;)

---
+++ src/Client/Client.fs
@@ -0,0 +1,24 @@
+﻿module Client
+
+open System.Net.Sockets
+open System.IO
+open System.Threading.Tasks
+
+let awaitTaskVoid : Task -> Async<unit> = Async.AwaitIAsyncResult >> Async.Ignore
+
+type Client() = 
+    let tcpClient = new TcpClient() // tcpClient is private since this is an internal let binding 
+    
+    member this.ConnectTo(host, port) = async { do! awaitTaskVoid (tcpClient.ConnectAsync(host = host, port = port)) } |> Async.RunSynchronously
+    member this.Write msg = 
+        let streamWriter = new StreamWriter(tcpClient.GetStream())
+        match tcpClient.Connected with
+        | true -> 
+            async { 
+                do! awaitTaskVoid (streamWriter.WriteLineAsync(msg.ToString()))
+                do! awaitTaskVoid (streamWriter.FlushAsync())
+            }
+            |> Async.RunSynchronously
+        | _ ->  streamWriter.Dispose()
+                failwith "client not connected"
+
