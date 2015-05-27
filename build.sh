440d0bf6a14e1a3ae2809fded0e062658ba7fcdc
Jallah <puncher@gmx.net>
2015-05-27 21:40:30 +0200

    still travis issues part2

--- build.sh
+++ build.sh
@@ -1,9 +1,20 @@
-!/bin/bash
+#!/bin/bash
+
 if test "$OS" = "Windows_NT"
 then
   # use .Net
+
+  if [ ! -f packages/FAKE/tools/FAKE.exe ]; then
+    .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
+  fi
+
   packages/FAKE/tools/FAKE.exe build.fsx 
 else
   # use mono
+
+  if [ ! -f packages/FAKE/tools/FAKE.exe ]; then
+    mono .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
+  fi
+
   mono packages/FAKE/tools/FAKE.exe build.fsx 
 fi
