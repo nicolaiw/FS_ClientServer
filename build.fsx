e6e9301d2ec2e30043a943faaf2fd47a3f8a8f90
Jallah <puncher@gmx.net>
2015-05-27 19:10:31 +0200

    add FAKE

---
+++ build.fsx
@@ -0,0 +1,24 @@
+﻿// include Fake lib
+#r "packages/FAKE/tools/FakeLib.dll"
+open Fake
+
+// Properties
+let buildDir = "./build/"
+
+// Targets
+Target "Clean" (fun _ ->
+    CleanDir buildDir
+)
+
+Target "BuildApp" (fun _ ->
+    !! "src/**/*.fsproj"
+      |> MSBuildRelease buildDir "Build"
+      |> Log "AppBuild-Output: "
+)
+
+// Dependencies
+"Clean"
+  ==> "BuildApp"
+
+// start build
+RunTargetOrDefault "BuildApp"
